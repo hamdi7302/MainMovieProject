0e4c0e9dba5807529c90669751b87bf7c6c84f78
Hamdi <hamdi.bntiba@gmail.com>
2024-09-26 20:45:14 +0100

    Initial Commit

---
+++ MainMovieProject/MainMovieProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainMovieProjectApp.swift
+//  MainMovieProject
+//
+//  Created by hamdi on 26/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct MainMovieProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
