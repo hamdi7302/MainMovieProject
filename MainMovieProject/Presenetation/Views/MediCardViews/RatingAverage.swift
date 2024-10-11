3cef485276ddf54e651e042e7e817d10e8f4a1e8
Hamdi <hamdi.bntiba@gmail.com>
2024-10-11 13:54:12 +0100

    change voteaVERAGE

---
+++ MainMovieProject/Presenetation/Views/MediCardViews/RatingAverage.swift
@@ -0,0 +1,18 @@
+//
+//  RatingAverage.swift
+//  MainMovieProject
+//
+//  Created by hamdi on 11/10/2024.
+//
+
+import SwiftUI
+
+struct RatingAverage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingAverage()
+}
